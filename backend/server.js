0b8883d9fe9f5e67df12adf9ec9be374b84c1b16
nawazsadiqu <nawazsadiqu@gmail.com>
2026-04-30 18:00:55 +0530

    Increased request payload limit for large data upload

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,8 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
